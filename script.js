89f806af968613da52a226328806430400f1595b
Steve McLaughlin <stevejrmc@gmail.com>
2023-02-28 12:13:11 +0000

    Menu draw open/close complete

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $(() => {
+  $(document).on("click", ".menu-closed", function () {
+    $(this).removeClass("menu-closed").addClass("menu-opened");
 
+    $("#menu-draw").css({ "left": 0 });
+  });
+
+  $(document).on("click", ".menu-opened", function () {
+    $(this).removeClass("menu-opened").addClass("menu-closed");
+
+    $("#menu-draw").css({ "left": "-40vw" });
+  });
 })
