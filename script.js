026ea456976daff2829ec3451fb095614cd8a437
Steve McLaughlin <stevejrmc@gmail.com>
2023-03-01 21:28:32 +0000

    Added hidden google translate widget; temporarily hacked a buggy connection to custom language selection tool

--- script.js
+++ script.js
@@ -12,25 +12,77 @@ $(() => {
 
     $("#menu-draw").css("left", "-40vw");
   }
-  const hide = id => $(`.${id}`).addClass("hide");
+  const hide = selector => $(`${selector}`).addClass("hide");
   const showBlog = () => {
     closeMenu();
-    hide("resume");
-    hide("projects");
+    hide(".resume");
+    hide(".projects");
     $(".blog").removeClass("hide");
   }
   const showResume = () => {
     closeMenu();
-    hide("blog");
-    hide("projects");
+    hide(".blog");
+    hide(".projects");
     $(".resume").removeClass("hide");
   }
-  const showProjects = function () {
+  const showProjects = () => {
     closeMenu();
-    hide("blog");
-    hide("resume");
+    hide(".blog");
+    hide(".resume");
     $(".projects").removeClass("hide");
   }
+  const showLangOptions = () => {
+    $("#lang-options").removeClass("hide");
+  }
+
+  // Thank you Marcelo Rossi: 
+  // https://stackoverflow.com/users/463329/marcelo-rossi
+  // Found this function on Stackoverflow:
+  function simulateClick(item) {
+    item.dispatchEvent(new PointerEvent('pointerdown', { bubbles: true }));
+    item.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }));
+    item.dispatchEvent(new PointerEvent('pointerup', { bubbles: true }));
+    item.dispatchEvent(new MouseEvent('mouseup', { bubbles: true }));
+    item.dispatchEvent(new MouseEvent('mouseout', { bubbles: true }));
+    item.dispatchEvent(new MouseEvent('click', { bubbles: true }));
+    item.dispatchEvent(new Event('change', { bubbles: true }));
+
+    return true;
+  }
+
+  const selectLanguage = function () {
+    let userChoice = this.id;
+    const options = {
+      chinese: 19,
+      french: 33,
+      portuguese: 92,
+      spanish: 109,
+      arabic: 4,
+      english: 28,
+    }
+    const abbrv = {
+      chinese: "CN",
+      french: "FR",
+      portuguese: "PT",
+      spanish: "ES",
+      english: "EN",
+      arabic: "AR"
+    };
+    let abbreviation = abbrv[userChoice];
+    let selection = options[userChoice];
+    console.log("selection: ", selection);
+    console.log("abbreviation: ", abbreviation);
+    console.log(document.querySelector(".goog-te-combo"))
+    const lang = document.querySelector(".goog-te-combo").children[selection];
+    lang.selected = true;
+    simulateClick(lang);
+    $("#lang").text(abbreviation);
+
+    hide("#lang-options");
+  };
+
+
+
 
   // Event handlers
   $(document).on("click", ".menu-closed", openMenu);
@@ -38,4 +90,6 @@ $(() => {
   $(".blog-link").on("click", showBlog);
   $(".resume-link").on("click", showResume);
   $(".projects-link").on("click", showProjects);
+  $("#head-right").on("click", showLangOptions);
+  $(".lang").on("click", selectLanguage);
 })
